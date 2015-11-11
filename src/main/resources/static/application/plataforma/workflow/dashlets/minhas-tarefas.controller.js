d60d3f0c83e22e73bd0417b83789a0db767c6671
viniciusksilva <viniciusksilva@gmail.com>
2015-11-11 15:59:38 -0200

    Sprint 5 - Dashlet de peticoes e tarefas criada

--- src/main/resources/static/application/plataforma/workflow/dashlets/minhas-tarefas.controller.js
+++ src/main/resources/static/application/plataforma/workflow/dashlets/minhas-tarefas.controller.js
@@ -6,28 +6,11 @@
 (function() {
 	'use strict';
 
-	angular.plataforma.controller('MinhasTarefasDashletController', ['$scope', 'TarefaService', 'PesquisaService', function($scope, TarefaService, PesquisaService) {
+	angular.plataforma.controller('MinhasTarefasDashletController', ['$scope', 'TarefaService', function($scope, TarefaService) {
 		
 		TarefaService.listar().success(function(tarefas) {
 			$scope.tarefas = tarefas;
-			angular.forEach(tarefas, function(tarefa) {
-				var command = PesquisarCommand(tarefa.metadado);
-				PesquisaService.pesquisar(command)
-				.then(function(resultados) {
-					tarefa.texto = resultados.data[0].objeto.identificacao;
-				});
-			});
 		});
-		
-    	function PesquisarCommand(metadado){
-    		var dto = {};
-    		dto.indices = ['autuacao', 'distribuicao'];
-    		dto.tipos = [metadado.tipoInformacao];
-    		dto.campos = ['identificacao'];
-    		dto.filtros = { 'id.sequencial' : metadado.informacao };
-
-    		return dto;
-    	}
 		
 	}]);
 	
