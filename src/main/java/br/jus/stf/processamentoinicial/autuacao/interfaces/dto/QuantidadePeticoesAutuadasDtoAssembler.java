5440d28cd0d00184d22afb66fd746092db2fc9d1
anderson-araujo <anderson.araujo@stf.jus.br>
2015-11-12 18:33:42 -0200

    Implementacao da pesquisa de indicadores e do evento PeticaoAutuada

---
+++ src/main/java/br/jus/stf/processamentoinicial/autuacao/interfaces/dto/QuantidadePeticoesAutuadasDtoAssembler.java
@@ -0,0 +1,12 @@
+package br.jus.stf.processamentoinicial.autuacao.interfaces.dto;
+
+/**
+ * Classe responsável por "montar" o DTO do indicador Quantidade de Petições Autuadas.
+ * 
+ * @author Anderson Araújo
+ * @since 09.11.2015
+ *
+ */
+public class QuantidadePeticoesAutuadasDtoAssembler {
+	
+}
