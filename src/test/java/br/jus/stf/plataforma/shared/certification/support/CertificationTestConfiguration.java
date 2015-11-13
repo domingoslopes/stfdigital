7bbcb4828524a9f73c9c0061e26375b651a4069c
Tomas.Godoi <tomas.godoi@stf.jus.br>
2015-11-13 16:09:52 -0200

    Trazendo o código do assinador.

---
+++ src/test/java/br/jus/stf/plataforma/shared/certification/support/CertificationTestConfiguration.java
@@ -0,0 +1,10 @@
+package br.jus.stf.plataforma.shared.certification.support;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("br.jus.stf.plataforma.shared.certification.support")
+public class CertificationTestConfiguration {
+
+}
