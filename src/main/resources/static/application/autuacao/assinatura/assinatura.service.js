f936fc8205c5fe86bf677410b97b997162a189f0
Tomas.Godoi <tomas.godoi@stf.jus.br>
2015-11-13 17:31:08 -0200

    Criando view para poder validar a integração com o open-eid.

---
+++ src/main/resources/static/application/autuacao/assinatura/assinatura.service.js
@@ -0,0 +1,14 @@
+/**
+ * @author Tomas.Godoi
+ * 
+ * @since 1.0.0
+ * @since 20.08.2015
+ */
+(function() {
+	'use strict';
+
+	angular.plataforma.service('AssinaturaService', ['$http', function($http) {
+		
+	}]);
+	
+})();
