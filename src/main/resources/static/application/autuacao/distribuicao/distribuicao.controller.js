36b721f6b83172ae9edf1516fc8cce5359a4d4e2
viniciusksilva <viniciusksilva@gmail.com>
2015-11-17 15:56:05 -0200

    Tipo de distribuição incluido, lista de processos da parte criada e lista de processo preventos criados

--- src/main/resources/static/application/autuacao/distribuicao/distribuicao.controller.js
+++ src/main/resources/static/application/autuacao/distribuicao/distribuicao.controller.js
@@ -11,7 +11,7 @@
 		
 		$scope.idPeticao = $stateParams.idTarefa;
 		
-		$scope.ministros = data.data;
+		$scope.ministrosCandidados = data.data;
 		
 		$scope.relator = '';
 		
@@ -20,15 +20,65 @@
 		$scope.processosParte = [];
 		
 		$scope.nomeParteRelacionada = '';
+		
+		$scope.tiposDistribuicao = [{id: 'COMUM', nome: 'Comum'}, {id: 'PREVENCAO', nome: 'Prevenção Relator/Sucessor'}];
+		
+		$scope.ministrosImpedidos = [];
+		
+		$scope.processo;
+		
+		$scope.processosPreventos = [];
+		
+		$scope.justificativa;
+		
+		$scope.$watch('processo', function(novo){
+			if (novo){
+				$scope.adicionarProcessoPreventoLista(novo);
+			}
+		});
+		
+		$scope.exibePainelMinistroImpedimento = false;
+		
+		$scope.exibeJustificativa = false;
+		
+		$scope.exibePesquisaProcessoPrevencao = false;
 
 		var partesPeticao = {};
 		
 		var peticao;
-		
-		//var idPessoa;
-		
+
 		var commandPartesPeticao;
 		
+		$scope.pesquisaProcesso = {
+				texto : 'identificacao',
+				indices : ['distribuicao'],
+				filtros : ['identificacao'],
+				ordenadores: {'identificacao' : 'ASC'},
+				pesquisa : 'sugestao'
+			};
+		
+		
+		$scope.adicionarProcessoPreventoLista = function(processo){
+			var verificaSeProcessoExiste = false;
+			angular.forEach($scope.processosPreventos, function(processoP) {
+				if (processoP.id == processo.id) {
+					verificaSeProcessoExiste = true;
+				}
+				if (processoP.objeto['relator.codigo'] != processo.objeto.relator.codigo){
+					verificaSeProcessoExiste = true
+					messages.error('Este processo possui um relator diferente do processo selecionado anteriormente!');
+				}
+			});
+			if (!verificaSeProcessoExiste){
+				$scope.processosPreventos.push(processo);
+				$scope.exibePesquisaProcessoPrevencao = true;
+				$scope.processo = null;
+			}
+		};
+		
+		$scope.removerProcessoPreventoLista = function($index){
+			$scope.processosPreventos.splice($index,1);
+		}
 		
 		PeticaoService.consultarPartes($scope.idPeticao).success(function(partesP) {
 			partesPeticao = partesP;
@@ -47,24 +97,72 @@
 			
 		});
 		
-		$scope.consultarProcesso = function(idPessoa, nomeParte){
-			$scope.nomeParteRelacionada = nomeParte;
-			var commandProcessosPessoa = new ProcessosDaParteCommand(idPessoa);
+		$scope.consultarProcesso = function(parte){
+			$scope.nomeParteRelacionada = parte.objeto.nome;
+			var commandProcessosPessoa = new ProcessosDaParteCommand(parte.id);
 			PesquisaService.pesquisar(commandProcessosPessoa).then(function(processos){
-				$scope.processosParte = processos.data;
+				parte.processosParte = processos.data;
 			}, function(processos,status){
 				messages.error('Ocorreu um erro e a pesquisa de processsos da parte não pode ser realizada!');
 			});
 		};
 		
+		$scope.verificaImpedimentoMinistro = function(tipoNome){
+			if (tipoNome == 'COMUM'){
+				$scope.exibePainelMinistroImpedimento = true;
+				$scope.exibePesquisaProcessoPrevencao = false;
+				$scope.processosParte = [];
+			}else if (tipoNome == 'PREVENCAO'){
+				$scope.exibePesquisaProcessoPrevencao = true;
+				$scope.exibePainelMinistroImpedimento = false;
+				$scope.ministrosCandidados = [];
+				$scope.ministrosImpedidos = [];
+			}else{
+				$scope.exibePainelMinistroImpedimento = false;
+				$scope.exibePesquisaProcessoPrevencao = false;
+			}
+		}
+		
+		$scope.formatarNomeMinistro = function(data){
+			return data.nome;
+		}
+		
 		
 		$scope.finalizar = function() {
-			if ($scope.relator.length === 0) {
-				messages.error('Você precisa selecionar um <b>ministro relator</b> para o processo.');
+			
+			//Caso o usuário selecione o tipo de distribuição "Comum", a lista de ministros candidatos não
+			//deve estar vazia
+			if (tipoDistribuicao.id == 'COMUM'){
+				if($scope.ministrosCandidados.length == 0){
+					messages.error('A lista deve possuir ao menos um Ministro para sorteio.');
+					return;
+				}
+			}
+			
+			//Caso o usuário selecione o tipo de distribuição "Prevenção", será necessário existir pelo menos
+			// um processo adicionado
+			if (tipoDistribuicao.id == 'PREVENCAO'){
+				if ($scope.processosPreventos.length == 0){
+					messages.error('Você precisa adicionar ao menos um processo na lista de preventos.');
+					return;
+				}
+			}
+			
+			//A justificativa deve ser obrigatória quando o tipo de distribuição for 'PREVENCAO'
+			if ($scope.justificativa.length == 0 && tipoDistribuicao.id == 'PREVENCAO' ){
+				messages.error('Você precisa inserir uma justificativa.');
 				return;
 			}
 			
-			var command = new DistribuirCommand($scope.relator);
+			//Se o tipo de distribuição for 'PREVENCAO' a lista de ministros candidatos e impedidos
+			//deverão estar vazias já que o relator do processo prevento será o relator da petição
+			if (tipoDistribuicao.id == 'PREVENCAO'){
+				$scope.ministrosCandidados = [];
+				$scope.ministrosImpedidos = [];
+			}
+			
+			var command = new DistribuirCommand($scope.tiposDistribuicao.id, $scope.peticaoId, $scope.justificativa, 
+					$scope.ministrosCandidados, $scope.ministrosImpedidos, $scope.processosPreventos);
 			
 			PeticaoService.distribuir($scope.idPeticao, command).success(function(data) {
 				$state.go('dashboard');
@@ -76,12 +174,17 @@
 			});
 		};
 		
-    	function DistribuirCommand(ministroId){
+		
+    	function DistribuirCommand(idTipoDistribuicao, idPeticao, justificativa, ministrosCandidatos, 
+    			ministrosImpedidos, processosPreventos){
     		var dto = {};
-    		
-    		dto.ministroId = ministroId;
-    		
-    		return dto;
+			dto.tipoDistribuicao = $scope.tiposDistribuicao.id;
+			dto.peticaoId = parseInt(idPeticao);
+			dto.justificativa = justificativa;
+			dto.ministrosCandidatos = ministrosCandidatos;
+			dto.ministrosImpedidos = ministrosImpedidos;
+			dto.processosPreventos = processosPreventos;
+			return dto;
     	}
     	
     	function PartesPeticaoCommand(idsPartes){
@@ -99,7 +202,7 @@
     		var dto = {};
     		
     		dto.indices = ['distribuicao'];
-    		dto.campos = ['id.sequencial', 'identificacao'];
+    		dto.campos = ['id.sequencial', 'identificacao', 'relator.codigo'];
     		dto.ordenadores = {'id.sequencial' : 'ASC'};
     		dto.filtros = { 'partes.pessoaId.sequencial': [idPessoa] };
 
