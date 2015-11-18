632abbf5317deff572116708f89d5f7e219d4dc0
Tomas.Godoi <tomas.godoi@stf.jus.br>
2015-11-18 14:47:09 -0200

    Criando a Custom PKI.

--- src/test/java/br/jus/stf/plataforma/shared/certification/support/pki/CustomPKIServiceTest.java
+++ src/test/java/br/jus/stf/plataforma/shared/certification/support/pki/CustomPKIServiceTest.java
@@ -1,24 +1,26 @@
-package br.jus.stf.plataforma.shared.certification.support;
+package br.jus.stf.plataforma.shared.certification.support.pki;
 
-import java.security.cert.X509Certificate;
-
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import br.jus.stf.plataforma.shared.certification.support.CertificationTestConfiguration;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = CertificationTestConfiguration.class)
-public class CustomPKITest {
+public class CustomPKIServiceTest {
 
 	@Autowired
-	private CustomPKI customCA;
-	
+	private CustomPKIService customPKIService;
+
 	@Test
 	public void testGerarCertificadoRaiz() throws Exception {
-		X509Certificate certificate = customCA.generateCertificate();
-		System.out.println(certificate);
+		CustomPKI customPKI = customPKIService.customPKI();
+		System.out.println(customPKI);
+		Assert.assertNotNull("CustomPKI deveria ter sido construído.", customPKI);
 	}
-	
+
 }
