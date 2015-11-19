e2fbf43e0a41d6b969e56a8402e4b05671ef2ffc
Rafael.Alencar <rafaelesdras@gmail.com>
2015-11-19 14:09:43 -0200

    Domínio de permissões.

---
+++ src/main/resources/db/migration/V1_0_19__Criacao_tabelas_permissao.sql
@@ -0,0 +1,18 @@
+create sequence plataforma.seq_usuario increment by 1 start with 1 nomaxvalue minvalue 1 nocycle nocache;
+create sequence plataforma.seq_tipo_segmento increment by 1 start with 1 nomaxvalue minvalue 1 nocycle nocache;
+create sequence plataforma.seq_tipo_informacao increment by 1 start with 1 nomaxvalue minvalue 1 nocycle nocache;
+create sequence plataforma.seq_segmento increment by 1 start with 1 nomaxvalue minvalue 1 nocycle nocache;
+create sequence plataforma.seq_permissao increment by 1 start with 1 nomaxvalue minvalue 1 nocycle nocache;
+
+create table plataforma.tipo_segmento (seq_tipo_segmento bigint not null, nom_tipo_segmento varchar2(100) not null, constraint pk_tipo_segmento primary key (seq_tipo_segmento));
+
+create table plataforma.tipo_informacao (seq_tipo_informacao bigint not null, nom_tipo_informacao varchar2(100) not null, constraint pk_tipo_informacao primary key (seq_tipo_informacao));
+
+create table plataforma.segmento (seq_segmento bigint not null, nom_segmento varchar2(100) not null, seq_tipo_informacao bigint not null, seq_tipo_segmento bigint, constraint pk_segmento primary key (seq_segmento));
+alter table plataforma.tipo_informacao add constraint FK_TIPO_INFORMACAO_SEGM foreign key (seq_tipo_informacao) references plataforma.tipo_informacao;
+alter table plataforma.tipo_segmento add constraint FK_TIPO_SEGMENTO_SEGM foreign key (seq_tipo_segmento) references plataforma.tipo_segmento;
+
+create table plataforma.permissao (seq_permissao bigint not null, seq_segmento bigint not null, tip_permissao varchar2(20), constraint pk_permissao primary key (seq_permissao));
+alter table plataforma.segmento add constraint FK_SEGMENTO_PERM foreign key (seq_segmento) references plataforma.segmento;
+
+create table plataforma.usuario (seq_usuario bigint not null, nom_usuario varchar2(100) not null, sig_usuario varchar2(30) not null, cod_cpf varchar2(11) not null, cod_oab varchar2(20), dsc_email varchar2(50) not null, dsc_telefone varchar2(14), constraint pk_usuario primary key (seq_usuario));
