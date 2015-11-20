f24e3fd206124f40b4b36df0bae52aacb114e45f
Rafael.Alencar <rafaelesdras@gmail.com>
2015-11-20 19:05:43 -0200

    Inclusão de chaves únicas.

--- src/main/resources/db/migration/V1_0_19__Criacao_tabelas_permissao.sql
+++ src/main/resources/db/migration/V1_0_19__Criacao_tabelas_permissao.sql
@@ -8,14 +8,18 @@ create sequence plataforma.seq_grupo increment by 1 start with 1 nomaxvalue minv
 create sequence plataforma.seq_papel increment by 1 start with 1 nomaxvalue minvalue 1 nocycle nocache;
 
 create table plataforma.tipo_segmento (seq_tipo_segmento bigint not null, nom_tipo_segmento varchar2(100) not null, constraint pk_tipo_segmento primary key (seq_tipo_segmento));
+alter table plataforma.tipo_segmento add constraint uk_nom_tipo_segmento_tise unique(nom_tipo_segmento);
 
 create table plataforma.tipo_informacao (seq_tipo_informacao bigint not null, nom_tipo_informacao varchar2(100) not null, constraint pk_tipo_informacao primary key (seq_tipo_informacao));
+alter table plataforma.tipo_informacao add constraint uk_nom_tipo_informacao_tiin unique(nom_tipo_informacao);
 
 create table plataforma.segmento (seq_segmento bigint not null, nom_segmento varchar2(100) not null, seq_tipo_informacao bigint not null, seq_tipo_segmento bigint, constraint pk_segmento primary key (seq_segmento));
+alter table plataforma.segmento add constraint uk_nom_segmento_segm unique(nom_segmento, seq_tipo_informacao, seq_tipo_segmento);
 alter table plataforma.tipo_informacao add constraint FK_TIPO_INFORMACAO_SEGM foreign key (seq_tipo_informacao) references plataforma.tipo_informacao;
 alter table plataforma.tipo_segmento add constraint FK_TIPO_SEGMENTO_SEGM foreign key (seq_tipo_segmento) references plataforma.tipo_segmento;
 
 create table plataforma.permissao (seq_permissao bigint not null, seq_segmento bigint not null, tip_permissao varchar2(20), constraint pk_permissao primary key (seq_permissao));
+alter table plataforma.permissao add constraint uk_seq_segmento_perm unique(seq_segmento, tip_permissao);
 alter table plataforma.segmento add constraint FK_SEGMENTO_PERM foreign key (seq_segmento) references plataforma.segmento;
 
 create table plataforma.usuario (seq_usuario bigint not null, nom_usuario varchar2(100) not null, sig_usuario varchar2(30) not null, cod_cpf varchar2(11) not null, cod_oab varchar2(20), dsc_email varchar2(50) not null, dsc_telefone varchar2(14), constraint pk_usuario primary key (seq_usuario));
@@ -26,6 +30,7 @@ alter table plataforma.permissao_usuario add constraint FK_USUARIO_PEUS foreign 
 alter table plataforma.permissao_usuario add constraint FK_PERMISSAO_PEUS foreign key (seq_permissao) references plataforma.permissao;
 
 create table plataforma.recurso (seq_recurso bigint not null, nom_recurso varchar2(50) not null, tip_recurso varchar2(20) not null, constraint pk_recurso primary key (seq_recurso));
+alter table plataforma.recurso add constraint uk_nom_recurso_recu unique(nom_recurso, tip_recurso);
 
 create table plataforma.permissao_recurso (seq_permissao bigint not null, seq_recurso bigint not null, constraint pk_permissao_recurso primary key (seq_permissao, seq_recurso));
 alter table plataforma.permissao_recurso add constraint FK_RECURSO_PERE foreign key (seq_recurso) references plataforma.recurso;
