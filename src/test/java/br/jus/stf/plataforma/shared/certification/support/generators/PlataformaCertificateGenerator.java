a71df48308b08b57726ba36fb5ec2953e58e0d9e
Tomas.Godoi <tomas.godoi@stf.jus.br>
2015-11-25 20:40:28 -0200

    Criando o mecanismo de geração de pki de teste.

---
+++ src/test/java/br/jus/stf/plataforma/shared/certification/support/generators/PlataformaCertificateGenerator.java
@@ -0,0 +1,16 @@
+package br.jus.stf.plataforma.shared.certification.support.generators;
+
+public class PlataformaCertificateGenerator {
+
+	/**
+	 * <pki-name> <person-name> <person-cpf>
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		for (String a : args) {
+			System.out.println(a);
+		}
+	}
+
+}
