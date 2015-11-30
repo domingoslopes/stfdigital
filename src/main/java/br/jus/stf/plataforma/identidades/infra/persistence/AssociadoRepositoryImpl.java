c8160172992060b35e6df9f1d41510c3a6df3984
Rafael.Alencar <rafaelesdras@gmail.com>
2015-11-30 15:06:59 -0200

    Repositório para associados. Exclusão de arquivos .orig.

---
+++ src/main/java/br/jus/stf/plataforma/identidades/infra/persistence/AssociadoRepositoryImpl.java
@@ -0,0 +1,35 @@
+package br.jus.stf.plataforma.identidades.infra.persistence;
+
+import javax.persistence.EntityManager;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.jus.stf.plataforma.identidades.domain.model.Associado;
+import br.jus.stf.plataforma.identidades.domain.model.AssociadoRepository;
+
+/**
+ * @author Rafael.Alencar
+ *
+ */
+@Repository
+public class AssociadoRepositoryImpl extends SimpleJpaRepository<Associado, Long> implements AssociadoRepository {
+
+	@Autowired
+	public AssociadoRepositoryImpl(EntityManager entityManager) {
+		super(Associado.class, entityManager);
+	}
+	
+	@Override
+	public Associado findOne(Long sequencial) {
+		return super.findOne(sequencial);
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public Associado save(Associado associado) {
+		return super.save(associado);
+	}
+
+}
