048d11d3eef1385d291be6daf38389985a3358b2
Rafael.Alencar <rafaelesdras@gmail.com>
2015-11-30 14:33:00 -0200

    Correção de testes. Inclusão de classes para Associação de usuários.

---
+++ src/main/java/br/jus/stf/plataforma/identidades/domain/model/TipoAssociado.java
@@ -0,0 +1,31 @@
+package br.jus.stf.plataforma.identidades.domain.model;
+
+import br.jus.stf.shared.stereotype.ValueObject;
+
+public enum TipoAssociado implements ValueObject<TipoAssociado> {
+	
+	ASSOCIADO("Associado"),
+	GESTOR("Gestor"),
+	REPRESENTANTE("Representante");
+	
+	private String descricao;
+	
+	private TipoAssociado(final String descricao) {
+		this.descricao = descricao;
+	}
+	
+	public String descricao() {
+		return descricao;
+	}
+	
+	@Override
+	public String toString() {
+		return descricao;
+	}
+
+	@Override
+	public boolean sameValueAs(final TipoAssociado other) {
+		return this.equals(other);
+	}
+
+}
--- src/test/java/br/jus/stf/plataforma/acessos/domain/model/PermissaoPapelUnitTests.java
+++ src/test/java/br/jus/stf/plataforma/acessos/domain/model/PermissaoPapelUnitTests.java
@@ -7,6 +7,9 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import br.jus.stf.shared.PapelId;
+import br.jus.stf.shared.SegmentoId;
+
 public class PermissaoPapelUnitTests {
 	
 	private Papel papel;
@@ -14,13 +17,13 @@ public class PermissaoPapelUnitTests {
 	@Before
 	public void setUp() {
 		TipoInformacao peticao = new TipoInformacao(1L, "Petição");
-		Segmento peticaoEletronica = new Segmento(1L, "Petição eletrônica", peticao);
+		Segmento peticaoEletronica = new Segmento(new SegmentoId(1L), "Petição eletrônica", peticao);
 		Permissao criarPeticaoEletronica = new Permissao(1L, TipoPermissao.CRIAR, peticaoEletronica);
 		TipoInformacao pessoa = new TipoInformacao(2L, "Pessoa");
-		Segmento cadastramentoPessoa = new Segmento(2L, "Cadastramento de pessoa", pessoa);
-		Segmento inclusaoPeca = new Segmento(3L, "Inclusão de peça", peticao);
+		Segmento cadastramentoPessoa = new Segmento(new SegmentoId(2L), "Cadastramento de pessoa", pessoa);
+		Segmento inclusaoPeca = new Segmento(new SegmentoId(3L), "Inclusão de peça", peticao);
 		Permissao criarPeca = new Permissao(3L, TipoPermissao.VISUALIZAR, inclusaoPeca);
-		Segmento pesquisaPessoa = new Segmento(4L, "Pesquisa de pessoa", pessoa);
+		Segmento pesquisaPessoa = new Segmento(new SegmentoId(4L), "Pesquisa de pessoa", pessoa);
 		Permissao pesquisarPessoa = new Permissao(5L, TipoPermissao.PESQUISAR, pesquisaPessoa);
 		Permissao criarPessoa = new Permissao(2L, TipoPermissao.CRIAR, cadastramentoPessoa);
 		Set<Permissao> permissoes = new HashSet<Permissao>();
@@ -30,19 +33,19 @@ public class PermissaoPapelUnitTests {
 		permissoes.add(criarPeca);
 		permissoes.add(pesquisarPessoa);
 		
-		papel = new Papel(1L, "Advogado");
+		papel = new Papel(new PapelId(1L), "Advogado");
 		papel.atribuirPermissoes(permissoes);
 	}
 	
 	@Test
 	public void papelPossuiAcessoNoRecurso() {
 		TipoInformacao peticao = new TipoInformacao(1L, "Petição");
-		Segmento peticaoEletronica = new Segmento(1L, "Petição eletrônica", peticao);
+		Segmento peticaoEletronica = new Segmento(new SegmentoId(1L), "Petição eletrônica", peticao);
 		Permissao criarPeticaoEletronica = new Permissao(1L, TipoPermissao.CRIAR, peticaoEletronica);
 		TipoInformacao pessoa = new TipoInformacao(2L, "Pessoa");
-		Segmento cadastramentoPessoa = new Segmento(2L, "Cadastramento de pessoa", pessoa);
+		Segmento cadastramentoPessoa = new Segmento(new SegmentoId(2L), "Cadastramento de pessoa", pessoa);
 		Permissao criarPessoa = new Permissao(2L, TipoPermissao.CRIAR, cadastramentoPessoa);		
-		Segmento inclusaoPeca = new Segmento(3L, "Inclusão de peça", peticao);
+		Segmento inclusaoPeca = new Segmento(new SegmentoId(3L), "Inclusão de peça", peticao);
 		Permissao criarPeca = new Permissao(3L, TipoPermissao.VISUALIZAR, inclusaoPeca);
 		Set<Permissao> permissoes = new HashSet<Permissao>();
 		
@@ -58,7 +61,7 @@ public class PermissaoPapelUnitTests {
 	@Test
 	public void papelNaoPossuiAcessoNoRecurso() {
 		TipoInformacao peticao = new TipoInformacao(1L, "Petição");
-		Segmento peticaoEletronica = new Segmento(1L, "Petição Eletrônica", peticao);
+		Segmento peticaoEletronica = new Segmento(new SegmentoId(1L), "Petição Eletrônica", peticao);
 		Permissao alterarPeticaoEletronica = new Permissao(4L, TipoPermissao.ALTERAR, peticaoEletronica);
 		Permissao pesquisarPeticaoEletronica = new Permissao(5L, TipoPermissao.PESQUISAR, peticaoEletronica);
 		Set<Permissao> permissoes = new HashSet<Permissao>();
