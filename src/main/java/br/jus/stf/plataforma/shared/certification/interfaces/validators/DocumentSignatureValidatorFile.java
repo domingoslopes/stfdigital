a839084ff792eea0d7b6469b1df99f7c71db2d01
Tomas.Godoi <tomas.godoi@stf.jus.br>
2015-12-02 23:46:39 -0200

    Fazendo alguns tratamentos para fechar InputStream.

--- src/main/java/br/jus/stf/plataforma/shared/certification/interfaces/validators/DocumentSignatureValidatorFile.java
+++ src/main/java/br/jus/stf/plataforma/shared/certification/interfaces/validators/DocumentSignatureValidatorFile.java
@@ -1,24 +1,27 @@
 package br.jus.stf.plataforma.shared.certification.interfaces.validators;
 
-import java.io.IOException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.InputStream;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
 import org.apache.commons.io.IOUtils;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.stereotype.Component;
 
-public class DocumentSignatureValidatorMultipartFile extends GenericSignatureValidator
-		implements ConstraintValidator<DocumentSigned, MultipartFile> {
+@Component
+public class DocumentSignatureValidatorFile extends GenericSignatureValidator
+		implements ConstraintValidator<DocumentSigned, File> {
 
 	@Override
-	public boolean isValid(MultipartFile value, ConstraintValidatorContext context) {
+	public boolean isValid(File value, ConstraintValidatorContext context) {
 		InputStream is = null;
 		try {
-			is = value.getInputStream();
+			is = new FileInputStream(value);
 			return isValid(is, context);
-		} catch (IOException e) {
+		} catch (FileNotFoundException e) {
 			throw new RuntimeException("Erro ao recuperar documento para validação.", e);
 		} finally {
 			IOUtils.closeQuietly(is);
