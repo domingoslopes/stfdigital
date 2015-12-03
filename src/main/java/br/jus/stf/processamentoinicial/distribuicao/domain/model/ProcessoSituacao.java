41ebe5d3e99b5af94a49a713099b676c1cc184d0
Rafael.Alencar <rafaelesdras@gmail.com>
2015-12-03 20:44:38 -0200

    Alteração de fluxo para permitir assinatura do documento de devolução.

--- src/main/java/br/jus/stf/processamentoinicial/distribuicao/domain/model/ProcessoSituacao.java
+++ src/main/java/br/jus/stf/processamentoinicial/distribuicao/domain/model/ProcessoSituacao.java
@@ -8,22 +8,7 @@ import br.jus.stf.shared.stereotype.ValueObject;
  */
 public enum ProcessoSituacao implements ValueObject<ProcessoSituacao> {
 	
-	DISTRIBUIDO("Distribuído");
-	
-	private String descricao;
-
-	private ProcessoSituacao(final String descricao) {
-		this.descricao = descricao;
-	}
-
-	public String descricao() {
-		return descricao;
-	}
-
-	@Override
-	public String toString() {
-		return descricao;
-	}
+	DISTRIBUIDO;
 
 	@Override
 	public boolean sameValueAs(final ProcessoSituacao other) {
