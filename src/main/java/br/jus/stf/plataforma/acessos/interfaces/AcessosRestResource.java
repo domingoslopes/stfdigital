c43ec38f20f99f9b5209da13b1a3dbd624aa072a
Gabriel Teles <gab.teles@hotmail.com>
2015-12-04 13:57:56 -0200

    Merge

--- src/main/java/br/jus/stf/plataforma/acessos/interfaces/AcessosRestResource.java
+++ src/main/java/br/jus/stf/plataforma/acessos/interfaces/AcessosRestResource.java
@@ -1,22 +1,37 @@
 package br.jus.stf.plataforma.acessos.interfaces;
 
+import java.util.LinkedHashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.jus.stf.plataforma.acessos.application.AcessosApplicationService;
 import br.jus.stf.plataforma.acessos.domain.model.Usuario;
+import br.jus.stf.plataforma.acessos.interfaces.commands.CadastrarUsuarioCommand;
+import br.jus.stf.plataforma.acessos.interfaces.commands.ConfigurarPermissoesUsuarioCommand;
+import br.jus.stf.plataforma.acessos.interfaces.dto.GrupoDto;
+import br.jus.stf.plataforma.acessos.interfaces.dto.GrupoDtoAssembler;
+import br.jus.stf.plataforma.acessos.interfaces.dto.PapelDto;
+import br.jus.stf.plataforma.acessos.interfaces.dto.PapelDtoAssembler;
 import br.jus.stf.plataforma.acessos.interfaces.dto.PermissaoDto;
 import br.jus.stf.plataforma.acessos.interfaces.dto.PermissaoDtoAssembler;
 import br.jus.stf.plataforma.acessos.interfaces.dto.UsuarioDto;
 import br.jus.stf.plataforma.acessos.interfaces.dto.UsuarioDtoAssembler;
 import br.jus.stf.plataforma.shared.security.SecurityContextUtil;
+
+import com.wordnik.swagger.annotations.ApiOperation;
 
 /**
  * @author Lucas.Rodrigues
@@ -35,6 +50,12 @@ public class AcessosRestResource {
 	@Autowired
 	private UsuarioDtoAssembler usuarioDtoAssembler;
 	
+	@Autowired
+	private GrupoDtoAssembler grupoDtoAssembler;
+	
+	@Autowired
+	private PapelDtoAssembler papelDtoAssembler; 
+	
 	@RequestMapping("/usuarios/permissoes")
 	public Set<PermissaoDto> permissoes(@RequestParam("login") String login) {
 		return acessosApplicationService.carregarPermissoesUsuario(login).stream()
@@ -50,16 +71,36 @@ public class AcessosRestResource {
 	}
 
 	@RequestMapping("/usuarios/papeis")
-	public Set<String> papeis(@RequestParam("login") String login) {
-		return acessosApplicationService.carregarPapeisUsuario(login);
+	public Set<PapelDto> papeis(@RequestParam("login") String login) {
+		return acessosApplicationService.carregarPapeisUsuario(login).stream()
+				.map(papel -> this.papelDtoAssembler.toDto(papel))
+				.sorted((p1, p2) -> p1.getNome().compareTo(p2.getNome()))
+				.collect(Collectors.toCollection(LinkedHashSet::new));
+	}
+	
+	@RequestMapping("/usuarios/grupos")
+	public Set<GrupoDto> grupos(@RequestParam("login") String login) {
+		return acessosApplicationService.carregarGruposUsuario(login).stream()
+				.map(grupo -> this.grupoDtoAssembler.toDto(grupo))
+				.sorted((g1, g2) -> g1.getNome().compareTo(g2.getNome()))
+				.collect(Collectors.toCollection(LinkedHashSet::new)); 
+	}
+	
+	@ApiOperation("Configura as permissões a grupos e papéis de um usuário.")
+	@RequestMapping(value = "/permissoes/configuracao", method = RequestMethod.POST)
+	@ResponseStatus(HttpStatus.OK)
+	public void configurarPermissoesUsuario(@RequestBody ConfigurarPermissoesUsuarioCommand command ){
+		this.acessosApplicationService.configurarPermissoesUsuario(command.getIdUsuario(), command.getPapeisAdicionados(), 
+				command.getGruposAdicionados(), command.getPapeisRemovidos(), command.getGruposRemovidos());
 	}
 	
 	/**
-	 * Recupera as informações do usuário.
+	 * Recupera as informações do usuário logado.
 	 * 
 	 * @param id Id do usuário.
 	 * @return Informações do usuário.
 	 */
+	@ApiOperation("Recupera as informações do usuário logado.")
 	@RequestMapping(value="/usuario", method = RequestMethod.GET)
 	public UsuarioDto recuperarInformacoes() {
 		
@@ -70,4 +111,17 @@ public class AcessosRestResource {
 		return this.usuarioDtoAssembler.toDto(usuario, authorities);
 	}
 	
+
+	/**
+	 * Cadastra um novo usuário
+	 * 
+	 * @param CadastrarUsuarioCommand command
+	 * @param BindingResult result
+	 * @return Long ID do usuário criado
+	 */
+	@ApiOperation("Cadastra um novo usuário")
+	@RequestMapping(value="/usuarios", method = RequestMethod.POST)
+	public Long cadastrarUsuario(@RequestBody @Valid CadastrarUsuarioCommand command, BindingResult result) {
+		return null;
+	}
 }
