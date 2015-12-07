d11f8097918fe684f56ae7454e4f310083aab6f0
Gabriel Teles <gab.teles@hotmail.com>
2015-12-07 17:39:40 -0200

    Tela de cadastro de usuário e testes

--- build/karma.config.js
+++ build/karma.config.js
@@ -30,8 +30,10 @@ module.exports = {
 	    baseDir + '/vendor/nvd3/build/nv.d3.js',
 	    baseDir + '/vendor/angular-nvd3/dist/angular-nvd3.js',
 	    baseDir + '/vendor/sockjs/sockjs.js',
-	    baseDir + '/vendor/stomp-websocket/lib/stomp.js',	
+	    baseDir + '/vendor/stomp-websocket/lib/stomp.js',
+	    baseDir + '/vendor/ngMask/dist/ngMask.min.js',
 	    baseDir + '/theme/assets/pages/js/pages.js',
+	    baseDir + '/theme/assets/plugins/jquery-validation/js/jquery.validate.min.js',
 		baseDir + '/application/plataforma/**/*.module.js', 
 		baseDir + '/application/plataforma/**/*.js', 
 		baseDir + '/application/autuacao/**/*.js', 
@@ -71,5 +73,5 @@ module.exports = {
 
 	urlRoot : '/__test/',
 
-	browsers : [ 'Chrome' ]
+	browsers : [ 'PhantomJS' ]
 };
