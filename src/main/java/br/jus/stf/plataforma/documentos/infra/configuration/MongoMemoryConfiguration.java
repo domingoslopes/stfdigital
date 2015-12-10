bdfe7b95f2e51918d2b6c62c04f180b210ab8424
Tomas.Godoi <tomas.godoi@stf.jus.br>
2015-12-10 02:37:27 -0200

    Permitindo manter os dados do mongodb no profile keepData combinado com desenvolvimento.

--- src/main/java/br/jus/stf/plataforma/documentos/infra/configuration/MongoMemoryConfiguration.java
+++ src/main/java/br/jus/stf/plataforma/documentos/infra/configuration/MongoMemoryConfiguration.java
@@ -6,13 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
 import org.springframework.data.mongodb.gridfs.GridFsTemplate;
 
-import br.jus.stf.plataforma.shared.settings.Profiles;
-
 import com.mongodb.Mongo;
 
+import br.jus.stf.plataforma.shared.settings.Profiles;
 import de.flapdoodle.embed.mongo.distribution.Version;
 import de.flapdoodle.embed.mongo.tests.MongodForTestsFactory;
 
@@ -26,8 +26,17 @@ public class MongoMemoryConfiguration extends AbstractMongoConfiguration {
 	private MongodForTestsFactory factory;
 	
 	@Autowired
-	public MongoMemoryConfiguration() throws IOException {
-		factory = MongodForTestsFactory.with(Version.Main.PRODUCTION);
+	private Environment env;
+	
+	public MongodForTestsFactory factory() throws IOException {
+		if (factory == null) {
+			if (env.acceptsProfiles(Profiles.KEEP_DATA)) {
+				factory = new PersistentMongodForTestsFactory();
+			} else {
+				factory = MongodForTestsFactory.with(Version.Main.PRODUCTION);
+			}
+		}
+		return factory;
 	}
 	
 	@Bean
@@ -42,7 +51,7 @@ public class MongoMemoryConfiguration extends AbstractMongoConfiguration {
 	
 	@Override
 	public Mongo mongo() throws Exception {
-		return factory.newMongo();
+		return factory().newMongo();
 	}
 	
 }
