23fdae3154be7bf5270d4de79df9a9183d23489f
Gabriel Teles <gab.teles@hotmail.com>
2015-12-11 17:06:29 -0200

    Modificado endpoint de órgãos; Frontend para cadastro de associados.

--- src/main/resources/static/application/plataforma/support/security/security.service.js
+++ src/main/resources/static/application/plataforma/support/security/security.service.js
@@ -28,6 +28,30 @@
 			return user;
 		};
 		
+		/**
+		 * Verifica se o usuário logado possui um papel através do nome do papel
+		 * 
+		 * @param string Nome do papel
+		 * @return boolean  
+		 */
+		this.hasPapel = function hasPapel(papelNome) {
+			var localUser = this.user();
+			
+			if (localUser === null) {
+				return false;
+			}
+
+			for (var i = 0; i < localUser.papeis.length; i++) {
+				var papel = localUser.papeis[i];
+				
+				if (papel.nome === papelNome) {
+					return true;
+				}
+			};
+			
+			return false;
+		}
+		
 		this.logout = function() {
 			return $http.post('/logout');
 		};
