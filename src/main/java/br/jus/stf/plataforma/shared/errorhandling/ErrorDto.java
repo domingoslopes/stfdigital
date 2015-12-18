01bcd9df483263534eeff00f8b4ffdd0f78e5969
Tomas.Godoi <tomas.godoi@stf.jus.br>
2015-12-18 18:22:42 -0200

    Tratando a validação do arquivo de peça no peticionamento.

---
+++ src/main/java/br/jus/stf/plataforma/shared/errorhandling/ErrorDto.java
@@ -0,0 +1,36 @@
+package br.jus.stf.plataforma.shared.errorhandling;
+
+public class ErrorDto {
+
+	private String name;
+	private String type;
+	private String message;
+
+	public ErrorDto() {
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+}
