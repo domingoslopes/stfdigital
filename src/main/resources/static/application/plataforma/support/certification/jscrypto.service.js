00026ff6c3063529a3fbedca7ff64617f28e6607
Tomas.Godoi <tomas.godoi@stf.jus.br>
2015-12-19 19:43:30 -0200

    Refatorando a correção do passo de assinatura com o JsCrypto.

--- src/main/resources/static/application/plataforma/support/certification/jscrypto.service.js
+++ src/main/resources/static/application/plataforma/support/certification/jscrypto.service.js
@@ -7,51 +7,13 @@
 (function() {
 	'use strict';
 
-	angular.plataforma.service('JsCryptoFactory', function($q) {
-		
-		var base64ToArrayBuffer = function(base64) {
-		    var binaryString =  window.atob(base64);
-		    var len = binaryString.length;
-		    var bytes = new Uint8Array(len);
-		    for (var i = 0; i < len; i++)        {
-		        bytes[i] = binaryString.charCodeAt(i);
-		    }
-		    return bytes.buffer;
-		};
-		
-		var arrayBufferToBase64 = function(buffer) {
-		    var binary = '';
-		    var bytes = new Uint8Array(buffer);
-		    var len = bytes.byteLength;
-		    for (var i = 0; i < len; i++) {
-		        binary += String.fromCharCode(bytes[i]);
-		    }
-		    return window.btoa(binary);
-		}
-		
-		var hex2ArrayBuffer = function(str) {
-            var len = Math.floor(str.length / 2);
-            var ret = new Uint8Array(len);
-            for (var i = 0; i < len; i++) {
-                ret[i] = parseInt(str.substr(i * 2, 2), 16);
-            }
-            return ret.buffer;
-	    };
-	    
-	    var arrayBuffer2hex = function(args) {
-	        var ret = "";
-	        var arr = new Uint8Array(args);
-	        for (var i = 0; i < arr.length; i++) {
-	        	ret += (arr[i] < 16 ? "0" : "") + arr[i].toString(16);
-	        }
-	        return ret.toLowerCase();
-	    };
+	angular.plataforma.service('JsCryptoFactory', function($q, Converter) {
 		
 		var Signature = function(sc) {
 			var signatureContent = sc;
 			
 			this.asHex = function() {
-				return arrayBuffer2hex(signatureContent);
+				return Converter.arrayBuffer2hex(signatureContent);
 			};
 			
 			this.asUint8Array = function() {
@@ -77,7 +39,7 @@
 							name: 'RSASSA-PKCS1-v1_5'
 						},
 						privateKey,
-						hex2ArrayBuffer(data.data)
+						Converter.hex2ArrayBuffer(data.data)
 					).then(function(sig) {
 						resolve(new Signature(sig));
 					}).catch(function(err) {
@@ -89,17 +51,11 @@
 		
 		this.create = function(privateKey, certificate) {
 			return $q(function(resolve, reject) {
-				console.log(privateKey);
-				window.crypto.subtle.importKey('jwk', privateKey, {
+				window.crypto.subtle.importKey('pkcs8', Converter.base64ToArrayBuffer(privateKey), {
 					name: 'RSASSA-PKCS1-v1_5',
 					hash: {name: 'SHA-256'}
 				}, true, ['sign']).then(function(pk) {
-					window.crypto.subtle.exportKey('jwk', pk).then(function(key) {
-						console.log(key);
-						resolve(new JsCrypto(pk, certificate));
-					}).catch(function(err) {
-						reject(err);
-					});
+					resolve(new JsCrypto(pk, certificate));
 				}).catch(function(err) {
 					reject(err);
 				});
