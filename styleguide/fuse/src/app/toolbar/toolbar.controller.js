10aa3b520a53c0bd0e8014189035a9570dfa420b
Gabriel Teles <gab.teles@hotmail.com>
2015-12-22 14:34:38 -0200

    Utilizando classy para o ToobarController

--- styleguide/fuse/src/app/toolbar/toolbar.controller.js
+++ styleguide/fuse/src/app/toolbar/toolbar.controller.js
@@ -1,129 +1,91 @@
-(function ()
-{
+(function () {
     'use strict';
 
-    angular
-        .module('app.toolbar')
-        .controller('ToolbarController', ToolbarController);
+    angular.module('app.toolbar').classy.controller({
+        name: 'ToolbarController',
 
-    /** @ngInject */
-    function ToolbarController($rootScope, $mdSidenav, $translate, $mdToast, $mdMedia, msNavigationService)
-    {
-        var vm = this;
+        inject: ['$rootScope', '$mdSidenav', '$mdToast', '$mdMedia', 'msNavigationService'],
 
-        // Data
-        vm.bodyEl = angular.element('body');
-        $rootScope.global = {
-            search: ''
-        };
-        vm.userStatusOptions = [
-            {
-                'title': 'Online',
-                'icon' : 'icon-checkbox-marked-circle',
-                'color': '#4CAF50'
+        init: function() {
+            this.bodyEl = angular.element('body');
+            
+            this.$rootScope.global = {
+                search: ''
+            };
+
+            this.userStatusOptions = [
+                {
+                    'title': 'Online',
+                    'icon' : 'icon-checkbox-marked-circle',
+                    'color': '#4CAF50'
+                },
+                {
+                    'title': 'Away',
+                    'icon' : 'icon-clock',
+                    'color': '#FFC107'
+                },
+                {
+                    'title': 'Do not Disturb',
+                    'icon' : 'icon-minus-circle',
+                    'color': '#F44336'
+                },
+                {
+                    'title': 'Invisible',
+                    'icon' : 'icon-checkbox-blank-circle-outline',
+                    'color': '#BDBDBD'
+                },
+                {
+                    'title': 'Offline',
+                    'icon' : 'icon-checkbox-blank-circle-outline',
+                    'color': '#616161'
+                }
+            ];
+
+            this.userStatus = this.userStatusOptions[0];
+        },
+
+        methods: {
+            /**
+             * 
+             */
+            toggleNavigation: function () { 
+                if (this.$mdMedia('gt-sm')) {
+                    this.msNavigationService.toggleFolded();
+                } else {
+                    this.toggleSidenav('navigation');
+                }
             },
-            {
-                'title': 'Away',
-                'icon' : 'icon-clock',
-                'color': '#FFC107'
+
+            /**
+             * Toggle sidenav
+             *
+             * @param sidenavId
+             */
+            toggleSidenav: function (sidenavId) {
+                this.$mdSidenav(sidenavId).toggle();
             },
-            {
-                'title': 'Do not Disturb',
-                'icon' : 'icon-minus-circle',
-                'color': '#F44336'
+
+            /**
+             * Sets User Status
+             * @param status
+             */
+            setUserStatus: function (status) {
+                this.userStatus = status;
             },
-            {
-                'title': 'Invisible',
-                'icon' : 'icon-checkbox-blank-circle-outline',
-                'color': '#BDBDBD'
+
+            /**
+             * Logout Function
+             */
+            logout: function () {
+
             },
-            {
-                'title': 'Offline',
-                'icon' : 'icon-checkbox-blank-circle-outline',
-                'color': '#616161'
+
+            /**
+             * Toggle horizontal mobile menu
+             */
+            toggleHorizontalMobileMenu: function () {
+                this.bodyEl.toggleClass('ms-navigation-horizontal-mobile-menu-active');
             }
-        ];
-
-        // Methods
-        vm.toggleNavigation = toggleNavigation;
-        vm.toggleSidenav = toggleSidenav;
-        vm.logout = logout;
-        vm.changeLanguage = changeLanguage;
-        vm.setUserStatus = setUserStatus;
-        vm.toggleHorizontalMobileMenu = toggleHorizontalMobileMenu;
-
-        //////////
-
-        vm.userStatus = vm.userStatusOptions[0];
-        
-        function toggleNavigation() {
-            if ($mdMedia('gt-sm')) {
-                msNavigationService.toggleFolded();
-            } else {
-                this.toggleSidenav('navigation');
-            }
         }
-
-        /**
-         * Toggle sidenav
-         *
-         * @param sidenavId
-         */
-        function toggleSidenav(sidenavId)
-        {
-            $mdSidenav(sidenavId).toggle();
-        }
-
-        /**
-         * Sets User Status
-         * @param status
-         */
-        function setUserStatus(status)
-        {
-            vm.userStatus = status;
-        }
-
-        /**
-         * Logout Function
-         */
-        function logout()
-        {
-
-        }
-
-        /**
-         * Change Language
-         */
-        function changeLanguage(lang)
-        {
-            vm.selectedLanguage = lang;
-
-            // Show temporary message if user selects a language other than English
-            if ( lang.code !== 'en' )
-            {
-                var message = 'Fuse supports translations through angular-translate module, but currently we do not have any translations other than English language. If you want to help us, send us a message through ThemeForest profile page.';
-
-                $mdToast.show({
-                    template : '<md-toast id="language-message" layout="column" layout-align="center start"><div class="md-toast-content">' + message + '</div></md-toast>',
-                    hideDelay: 7000,
-                    position : 'top right',
-                    parent   : '#content'
-                });
-
-                return;
-            }
-
-            //Change the language
-            $translate.use(lang.code);
-        }
-
-        /**
-         * Toggle horizontal mobile menu
-         */
-        function toggleHorizontalMobileMenu()
-        {
-            vm.bodyEl.toggleClass('ms-navigation-horizontal-mobile-menu-active');
-        }
-    }
-
+    });    
 })();
--- styleguide/fuse/src/app/toolbar/toolbar.module.js
+++ styleguide/fuse/src/app/toolbar/toolbar.module.js
@@ -3,12 +3,11 @@
     'use strict';
 
     angular
-        .module('app.toolbar', [])
+        .module('app.toolbar', ['classy'])
         .config(config);
 
     /** @ngInject */
-    function config($translatePartialLoaderProvider)
-    {
+    function config($translatePartialLoaderProvider) {
         $translatePartialLoaderProvider.addPart('app/toolbar');
     }
 })();
