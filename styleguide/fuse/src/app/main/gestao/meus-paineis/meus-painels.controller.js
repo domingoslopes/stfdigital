e9185b1d5b8c56be7fc9281ccbaca147f72c5f73
Gabriel Teles <gab.teles@hotmail.com>
2016-01-07 18:06:59 -0200

    Diretiva para gerar header automaticamente

---
+++ styleguide/fuse/src/app/main/gestao/meus-paineis/meus-painels.controller.js
@@ -0,0 +1,21 @@
+(function() {
+	'use strict';
+
+	var app = angular.module('app.gestao.meus-paineis');
+
+	app.classy.controller({
+		name: 'GestaoMeusPaineisController',
+
+		inject: [],
+
+		init: function() {
+
+		},
+
+		methods: {
+			foo: function() {
+				console.log("bar");
+			}
+		}
+	})
+})();
