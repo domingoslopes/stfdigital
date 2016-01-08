8b7d27ed4a87bc43a7d7a203ce858d1f34860ef3
Rafael.Alencar <rafaelesdras@gmail.com>
2016-01-08 18:18:39 -0200

    Classes auxiliares para tratamento de processos Recursais.

---
+++ src/main/java/br/jus/stf/jurisprudencia/controletese/domain/model/Tese.java
@@ -0,0 +1,119 @@
+package br.jus.stf.jurisprudencia.controletese.domain.model;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+import org.apache.commons.lang3.Validate;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
+import br.jus.stf.shared.AssuntoId;
+import br.jus.stf.shared.TeseId;
+
+@Entity
+@Table(name = "TESE", schema = "JURISPRUDENCIA", uniqueConstraints = @UniqueConstraint(columnNames = {"DSC_TESE", "TIP_TESE"}))
+public class Tese implements br.jus.stf.shared.stereotype.Entity<Tese, TeseId> {
+	
+	@EmbeddedId
+	private TeseId codigo;
+	
+	@Column(name = "DSC_TESE", nullable = false)
+	private String descricao;
+	
+	@Column(name = "TIP_TESE", nullable = false)
+	@Enumerated(EnumType.STRING)
+	private TipoTese tipo;
+	
+	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	@JoinTable(name = "TESE_ASSUNTO", schema = "JURISPRUDENCIA",
+		joinColumns = @JoinColumn(name = "SEQ_TESE", nullable = false),
+		inverseJoinColumns = @JoinColumn(name = "COD_ASSUNTO", nullable = false))
+	private Set<Assunto> assuntos = new HashSet<Assunto>(0);
+	
+	Tese() {
+		
+	}
+	
+	public Tese(final TeseId codigo, final String descricao) {
+		Validate.notNull(codigo, "tese.codigo.required");
+		Validate.notBlank(descricao, "tese.descricao.required");
+		
+		this.codigo = codigo;
+		this.descricao = descricao;
+	}
+	
+	public String descricao() {
+		return descricao;
+	}
+	
+	public Set<Assunto> assuntos() {
+		return Collections.unmodifiableSet(assuntos);
+	}
+	
+	public void atribuirAssuntos(final Set<Assunto> assuntos) {
+		Validate.notEmpty(assuntos, "tese.assuntos.required");
+		
+		this.assuntos.addAll(assuntos);
+	}
+	
+	public void removerAssuntos(final Set<AssuntoId> assuntos) {
+		Validate.notEmpty(assuntos, "tese.assuntos.required");
+		
+		Iterator<Assunto> assuntoIterator = this.assuntos.iterator();
+		
+		while(assuntoIterator.hasNext()) {
+			Assunto assunto = assuntoIterator.next();
+			
+			if (assuntos.contains(assunto.id())) {
+				assuntoIterator.remove();
+			}
+		}
+	}
+	
+	@Override
+	public TeseId id() {
+		return this.codigo;
+	}
+	
+	@Override
+	public int hashCode() {
+		return new HashCodeBuilder().append(codigo).hashCode();
+	}
+	
+	@Override
+	public boolean equals(Object obj){
+		
+		if (this == obj) {
+			return true;
+		}
+		
+		if (obj == null || getClass() != obj.getClass()) {
+			return false;
+		}
+	
+		Tese other = (Tese) obj;
+		
+		return codigo.equals(other.codigo);
+	}
+
+	@Override
+	public boolean sameIdentityAs(final Tese other) {
+		return other != null
+				&& codigo.equals(other.codigo);
+	}
+
+}
