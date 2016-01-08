ff8f4ab76dc270778df04ef19ad34ff654c412e3
Rafael.Alencar <rafaelesdras@gmail.com>
2016-01-08 18:18:39 -0200

    Classes auxiliares para tratamento de processos Recursais.

---
+++ src/main/java/br/jus/stf/shared/TeseId.java
@@ -0,0 +1,69 @@
+package br.jus.stf.shared;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+import org.apache.commons.lang3.Validate;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
+import br.jus.stf.shared.stereotype.ValueObject;
+
+/**
+ * @author Rafael.Alencar
+ * @version 1.0
+ * @created 08-jan-2016
+ */
+@Embeddable
+public class TeseId implements ValueObject<TeseId>{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Column(name = "COD_TESE", nullable = false)
+	private Long codigo;
+
+	TeseId() {
+
+	}
+
+	public TeseId(final Long codigo){
+		Validate.notNull(codigo, "teseId.codigo.required");
+		
+		this.codigo = codigo;
+	}
+
+	public Long toLong(){
+		return codigo;
+	}
+	
+	@Override
+	public String toString(){
+		return codigo.toString();
+	}
+
+	@Override
+	public int hashCode() {
+		return new HashCodeBuilder().append(codigo).toHashCode();
+	}
+	
+	@Override
+	public boolean equals(final Object o){
+		
+		if (this == o) {
+			return true;
+		}
+		
+		if (o == null || getClass() != o.getClass()) {
+			return false;
+		}
+		
+		TeseId other = (TeseId) o;
+		
+		return sameValueAs(other);
+	}
+
+	@Override
+	public boolean sameValueAs(final TeseId other){
+		return other != null && this.codigo.equals(other.codigo);
+	}
+	
+}
