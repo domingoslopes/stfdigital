3f24e56f65ef76c832b015aecc924c9a69c32a7d
Gabriel Teles <gab.teles@hotmail.com>
2016-01-15 15:06:51 -0200

    Gestão > Meus Paineis > Petições completo

--- styleguide/fuse/src/app/main/gestao/meus-paineis/meus-paineis.module.js
+++ styleguide/fuse/src/app/main/gestao/meus-paineis/meus-paineis.module.js
@@ -8,7 +8,7 @@
 	/** @ngInject **/
 	function config($translatePartialLoaderProvider, $stateProvider, msNavigationServiceProvider) {
 		$translatePartialLoaderProvider.addPart('app/main/gestao/meus-paineis');
-		
+
 		$stateProvider
 			.state('app.gestao.meus-paineis', {
 				url: '/meus-paineis',
@@ -17,14 +17,46 @@
 						templateUrl: 'app/main/gestao/meus-paineis/meus-paineis.html',
 						controller: 'GestaoMeusPaineisController',
 						controllerAs: 'vm'
-					}
+					},
+                    'principal@app.gestao.meus-paineis': {
+                        templateUrl: 'app/main/gestao/meus-paineis/principal/principal.html',
+                        controller: 'GestaoMeusPaineisPrincipalController',
+                        controllerAs: 'vm'
+                    },
+                    'peticoes@app.gestao.meus-paineis': {
+                        templateUrl: 'app/main/gestao/meus-paineis/peticoes/peticoes.html',
+                        controller: 'GestaoMeusPaineisPeticoesController',
+                        controllerAs: 'vm'
+                    },
+                    'autuacoes@app.gestao.meus-paineis': {
+                        templateUrl: 'app/main/gestao/meus-paineis/autuacoes/autuacoes.html',
+                        //controller: 'GestaoMeusPaineisAutuacoesController',
+                        //controllerAs: 'vm'
+                    },
+                    'produtividade-do-time@app.gestao.meus-paineis': {
+                        templateUrl: 'app/main/gestao/meus-paineis/produtividade-do-time/produtividade-do-time.html',
+                        //controller: 'GestaoMeusPaineisProdutividadeDoTimeController',
+                        //controllerAs: 'vm'
+                    }
 				}
-			});
+			})
+            .state('app.gestao.meus-paineis.principal', {
+                url: '/principal',
+            })
+            .state('app.gestao.meus-paineis.peticoes', {
+                url: '/peticoes',
+            })
+            .state('app.gestao.meus-paineis.autuacoes', {
+                url: '/autuacoes',
+            })
+            .state('app.gestao.meus-paineis.produtividade-do-time', {
+                url: '/produtividade-do-time',
+            });
 
         msNavigationServiceProvider.saveItem('gestao.meus-paineis', {
             title      : 'Meus Paineis',
             icon       : 'icon-view-dashboard',
-            state      : 'app.gestao.meus-paineis.*',
+            state      : 'app.gestao.meus-paineis',
             translation: 'GESTAO.MEUS-PAINEIS.MEUS-PAINEIS',
             weight     : 1
         });
