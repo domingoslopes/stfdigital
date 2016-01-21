649aa6c94374cb1d397a573f6fbc918eea80bae7
lucas-a-rodrigues <lucas.rodrigues@stf.jus.br>
2016-01-21 18:30:39 -0200

    Correção de testes e2e

--- src/main/resources/static/application/test/e2e/peticaodigitalorgao.scenario.js
+++ src/main/resources/static/application/test/e2e/peticaodigitalorgao.scenario.js
@@ -99,7 +99,7 @@
 		    	pos = text.search("#");
 		    	pos = pos + 1;
 		    	peticaoId = text.substr(pos, text.length);
-		    	expect(principalPage.tarefas().get(0).getText()).toEqual('Autuar Processo #' + peticaoId);
+		    	expect(principalPage.tarefas().get(0).getText()).toEqual('Autuar Processo Originário #' + peticaoId);
 		    });
 			
 		    principalPage.executarTarefa();
