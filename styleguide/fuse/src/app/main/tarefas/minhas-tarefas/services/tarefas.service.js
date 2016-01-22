eae0c84ca0923a4370ed63e04600f3a2fb5824e0
Gabriel Teles <gab.teles@hotmail.com>
2016-01-22 14:13:56 -0200

    Base da tela Tarefas > Minhas Tarefas

---
+++ styleguide/fuse/src/app/main/tarefas/minhas-tarefas/services/tarefas.service.js
@@ -0,0 +1,26 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('app.tarefas.minhas-tarefas')
+        .factory('TarefasService', TarefasService);
+
+    /** @ngInject */
+    function TarefasService($q, $http) {
+        return {
+            get: function() {
+                var deferred = $q.defer();
+
+                $http.get('app/data/sample/tarefas/minhas-tarefas/tarefas.json').then(function (response) {
+                    deferred.resolve(response.data);
+                }, function (response) {
+                    deferred.reject(response);
+                });
+
+                return deferred.promise;
+            }
+        }
+
+        return service;
+    }
+})();
