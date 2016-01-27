3a4fbdfd16ed954a5409f1d0910634cf3e3e115d
Anderson.Araujo <anderson.araujo@stf.jus.br>
2016-01-27 14:55:05 -0200

    Implementação da analise de pressupostos formais

---
+++ src/main/resources/db/migration/V1_0_51__Adic_usuario_analista-pressupostos.sql
@@ -0,0 +1,19 @@
+INSERT INTO corporativo.pessoa (seq_pessoa, nom_pessoa, cod_cpf, cod_oab, dsc_email, dsc_telefone) VALUES (corporativo.seq_pessoa.nextval, 'analista de pressupostos', '14102251006', null, 'apress@teste.com.br', '61-80801215');
+
+INSERT INTO plataforma.usuario (seq_usuario, sig_usuario, seq_pessoa, cod_setor_lotacao) VALUES (plataforma.seq_usuario.nextval, 'analista-pressupostos', corporativo.seq_pessoa.currval, 600000627);
+
+INSERT INTO plataforma.grupo_usuario(seq_grupo, seq_usuario) VALUES (1, plataforma.seq_usuario.currval);
+
+INSERT INTO plataforma.papel(seq_papel, nom_papel, seq_grupo) VALUES (16, 'analista-pressupostos', 2);
+INSERT INTO plataforma.papel_usuario(seq_papel, seq_usuario) VALUES (16, plataforma.seq_usuario.currval);
+
+INSERT INTO plataforma.segmento(seq_segmento, nom_segmento, seq_tipo_informacao) VALUES (28, 'ANALISE-PRESSUPOSTOS', 5);
+INSERT INTO plataforma.permissao(seq_permissao, seq_tipo_informacao, seq_segmento, tip_permissao) VALUES (37, 5, 28, 'EXECUTAR');
+
+INSERT INTO plataforma.permissao_papel (seq_permissao, seq_papel) VALUES (7, 16);  /* DASHLET   -> TAREFA                  -> VISUALIZAR */  
+INSERT INTO plataforma.permissao_papel (seq_permissao, seq_papel) VALUES (29, 16); /* DASHBORAD -> AUTUACAO                -> VISUALIZAR */
+INSERT INTO plataforma.permissao_papel (seq_permissao, seq_papel) VALUES (33, 16); /* TAREFA    -> ASSUMIR                 -> VISUALIZAR */
+INSERT INTO plataforma.permissao_papel (seq_permissao, seq_papel) VALUES (37, 16); /* TAREFA    -> ANÁLISE DE PRESSUPOSTOS -> EXECUTAR   */
+
+INSERT INTO plataforma.recurso (seq_recurso, nom_recurso, tip_recurso) VALUES (31, 'analisar-pressupostos', 'ACAO');
+INSERT INTO plataforma.permissao_recurso (seq_permissao, seq_recurso) VALUES (37, 31);
