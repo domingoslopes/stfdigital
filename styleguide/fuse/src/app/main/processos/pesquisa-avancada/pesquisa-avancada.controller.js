e467e2dd9a63ab136c5a04c97558283f2cbf7c04
Gabriel Teles <gab.teles@hotmail.com>
2016-01-28 15:44:23 -0200

    Ajustes na tela de Processos > Pesquisa Avançada

--- styleguide/fuse/src/app/main/processos/pesquisa-avancada/pesquisa-avancada.controller.js
+++ styleguide/fuse/src/app/main/processos/pesquisa-avancada/pesquisa-avancada.controller.js
@@ -8,26 +8,10 @@
 
         name: 'ProcessosPesquisaAvancadaController',
 
-        inject: ['$mdDialog', '$scope', '$document', 'traits'],
+        inject: ['$mdDialog', '$scope', '$document', '$filter', '$mdToast', 'traits'],
 
         init: function() {
-
-            this.logicalOperators = ['E', 'OU', 'NAO'];
-            this.comparisonOperators = {
-                'IGUAL': ['string', 'number', 'currency', 'date', 'list'],
-                'CONTEM': ['string'],
-                'ENTRE': ['number', 'currency', 'date'],
-                'MAIOR-QUE': ['number', 'currency', 'date'],
-                'MENOR-QUE': ['number', 'currency', 'date'],
-                'EXISTE': ['constant', 'string', 'number', 'currency', 'date', 'list']
-            };
-
-            this.traitSearchText = '';
-            this.newCriteria = {
-                operator: 'E',
-                trait: null
-            };
-
+            this.translate = this.$filter('translate');
             this.traits = this.traits.data;
 
             this.search = {
@@ -36,80 +20,30 @@
                 criterias: []
             };
             
-            this.criteriaOrder = '';
-            this.sortableOptions = {
-                ghostClass: 'criteria-item-placeholder',
-                handle: '.handle',
-                forceFallback: true,
-                fallbackClass: 'criteria-item-ghost'
-            };
-
-           
+            this.selectedTab = false;
+            this.searchComplete = false;   
         },
 
         methods: {
-
-            setCriteriaLogicalOperator: function(criteria, operator) {
-                criteria.logicalOperator = operator;
+            canSearch: function() {
+                return ((this.search.criterias.length > 0) && (_.all(this.search.criterias, 'valid')));
             },
 
-            setAsFavorite: function(criteria) {
-                criteria.isFavorite = !criteria.isFavorite;
+            doSearch: function() {
+                // TODO: Do search
+                this.selectedTab = 1;
+                this.currentSearch = this.search;
+                this.searchComplete = true;
             },
 
-            removeCriteria: function(i) {
-                _.pullAt(this.search.criterias, i);
-            },
-
-            querySearch: function(query) {
-                var results = query ? _.filter(this.traits, this._createFilterFor(query)) : this.traits;
-                return results;
-            },
-
-            searchTextChange: function(text) {
-                console.log('Text changed to ' + text);
-            },
-
-            selectedItemChange: function(item) {
-                console.log('Item changed to ' + JSON.stringify(item));
-            },
-
-            addNewCriteria: function() {
-                var trait = this.newCriteria.trait;
-                var criteria = {
-                    id: null,
-                    logicalOperator: this.newCriteria.operator,
-                    comparisonOperator: trait.dataType === 'constant' ? 'EXISTS' : 'EQUALS',
-                    trait: trait,
-                    value: trait.dataType === 'constant' ? trait.name : null
-                };
-                this.traitSearchText = '';
-                this.newCriteria.trait = null;
-                this.search.criterias.push(criteria);
-            },
-
-            /**
-             * Get the valid comparison operator for an specific data type.
-             * @param {String} dataType
-             * @return {Array} Valid comparison operators
-             */
-            getComparisonOperators: function(dataType) {
-                var operatorMapping = this.comparisonOperators;
-                var ops = _.keys(operatorMapping);
-                return _.reject(ops, function(op) {
-                    var types = operatorMapping[op];
-                    return !_.include(types, dataType);
-                });
-            },
-
-            /**
-             * Create filter function for a query string
-             */
-            _createFilterFor: function(query) {
-                var lowercaseQuery = angular.lowercase(query);
-                return function filterFn(trait) {
-                    return (trait.name.toLowerCase().indexOf(lowercaseQuery) !== -1);
-                };
+            saveSearch: function() {
+                // TODO: Save search
+                this.$mdToast.show(
+                    this.$mdToast.simple()
+                        .textContent(this.translate('PROCESSOS.PESQUISA-AVANCADA.PESQUISA-SALVA'))
+                        .position('top right')
+                        .hideDelay(3000)
+                );
             }
 
         }
