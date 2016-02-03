905f95f1cc54bb629245aef1f109e2bb6e501c39
Gabriel Teles <gab.teles@hotmail.com>
2016-02-03 17:26:25 -0200

    Revisão de repercussão geral

---
+++ src/main/java/br/jus/stf/plataforma/shared/web/JacksonConfiguration.java
@@ -0,0 +1,23 @@
+package br.jus.stf.plataforma.shared.web;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+/** 
+ * @author Gabriel.Bastos
+ *
+ */
+
+@Configuration
+public class JacksonConfiguration {
+
+	@Bean public ObjectMapper objectMapper() {
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.configure(MapperFeature.DEFAULT_VIEW_INCLUSION, true);
+		return mapper;
+	}
+	
+}
