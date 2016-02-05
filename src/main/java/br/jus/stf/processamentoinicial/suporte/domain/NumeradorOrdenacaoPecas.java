9d1a658723046b2c0201809013b7871da337882c
Tomas.Godoi <tomas.godoi@stf.jus.br>
2016-02-05 17:06:22 -0200

    [PLATAFORMA-481]
    Implementando a reordenação de peças.

--- src/main/java/br/jus/stf/processamentoinicial/suporte/domain/NumeradorOrdenacaoPecas.java
+++ src/main/java/br/jus/stf/processamentoinicial/suporte/domain/NumeradorOrdenacaoPecas.java
@@ -1,6 +1,7 @@
 package br.jus.stf.processamentoinicial.suporte.domain;
 
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.Iterator;
 
 import br.jus.stf.processamentoinicial.suporte.domain.model.Peca;
@@ -45,12 +46,16 @@ public class NumeradorOrdenacaoPecas {
 	 * 
 	 */
 	public void normalizarOrdenacaoPecas() {
-		Iterator<Peca> iterator = pecas.stream().sorted((p1, p2) -> p1.numeroOrdem().compareTo(p2.numeroOrdem())).iterator();
+		Iterator<Peca> iterator = pecas.stream().sorted(comparatorPecasPeloNumeroOrdem()).iterator();
 		Long numeroOrdemAtual = 1L;
 		while (iterator.hasNext()) {
 			iterator.next().numerarOrdem(numeroOrdemAtual);
 			numeroOrdemAtual++;
 		}
+	}
+
+	private Comparator<? super Peca> comparatorPecasPeloNumeroOrdem() {
+		return (p1, p2) -> p1.numeroOrdem().compareTo(p2.numeroOrdem());
 	}
 
 	/**
@@ -72,5 +77,36 @@ public class NumeradorOrdenacaoPecas {
 		pecaSubstituta.numerarOrdem(pecaOriginal.numeroOrdem());
 	}
 	
+	/**
+	 * Reordena uma peça para o novo número de ordem, caso seja possível.
+	 * 
+	 * @param peca
+	 * @param novoNumeroOrdem
+	 * @return true se conseguiu reordenar, false caso contrário
+	 */
+	public boolean reordenarPeca(Peca peca, Long novoNumeroOrdem) {
+		if (!pecas.contains(peca)) {
+			return false; // Peça não está na coleção.
+		}
+		if (!pecas.stream().anyMatch(p -> p.numeroOrdem().equals(novoNumeroOrdem))) {
+			return false; // Não possui um item com o novo número de ordem.
+		}
+		Iterator<Peca> iterator = pecas.stream().sorted(comparatorPecasPeloNumeroOrdem()).iterator();
+		Long numeroOrdemAtual = 1L;
+		Long numeroOrdemFinal = new Long(pecas.size());
+		while (numeroOrdemAtual <= numeroOrdemFinal) {
+			if (numeroOrdemAtual == novoNumeroOrdem) { // Numera a peça com o novo número de ordem.
+				peca.numerarOrdem(numeroOrdemAtual);
+				numeroOrdemAtual++;
+			} else { // Numera as outras peças na sequência.
+				Peca pecaAtual = iterator.next();
+				if (!pecaAtual.equals(peca)) {
+					pecaAtual.numerarOrdem(numeroOrdemAtual);
+					numeroOrdemAtual++;
+				}
+			}
+		}
+		return true;
+	}
 	
 }
