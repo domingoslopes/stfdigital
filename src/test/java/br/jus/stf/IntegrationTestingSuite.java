6b934b9ea414e863f293c9353281fff440698638
Tomas.Godoi <tomas.godoi@stf.jus.br>
2016-02-10 17:07:14 -0200

    Renomeando alguns testes de integração.
    Adicionando a dependência do junit-toolbox.

--- src/test/java/br/jus/stf/IntegrationTestingSuite.java
+++ src/test/java/br/jus/stf/IntegrationTestingSuite.java
@@ -9,8 +9,8 @@ import br.jus.stf.plataforma.actions.AcoesIntegrationTests;
 import br.jus.stf.plataforma.dashboards.DashboardIntegrationTests;
 import br.jus.stf.plataforma.documentos.DocumentoIntegrationTests;
 import br.jus.stf.plataforma.pesquisas.PesquisaIntegrationTests;
-import br.jus.stf.plataforma.shared.certification.application.DocumentValidatorApplicationServiceTest;
-import br.jus.stf.plataforma.shared.certification.application.SignatureApplicationServiceTest;
+import br.jus.stf.plataforma.shared.certification.application.DocumentValidatorApplicationServiceIntegrationTests;
+import br.jus.stf.plataforma.shared.certification.application.SignatureApplicationServiceIntegrationTests;
 import br.jus.stf.processamentoinicial.autuacao.interfaces.AutuacaoOriginariosIntegrationTests;
 import br.jus.stf.processamentoinicial.autuacao.interfaces.actions.PeticionamentoActionIntegrationTests;
 import br.jus.stf.processamentoinicial.distribuicao.application.ProcessoApplicationEventIntegrationTests;
@@ -27,8 +27,8 @@ import br.jus.stf.processamentoinicial.distribuicao.interfaces.ConsultaProcessos
 	AcoesIntegrationTests.class,
 	DashboardIntegrationTests.class,
 	PeticionamentoActionIntegrationTests.class,
-	SignatureApplicationServiceTest.class,
-	DocumentValidatorApplicationServiceTest.class,
+	SignatureApplicationServiceIntegrationTests.class,
+	DocumentValidatorApplicationServiceIntegrationTests.class,
 	TeseIntegrationTests.class
 })
 public class IntegrationTestingSuite {
