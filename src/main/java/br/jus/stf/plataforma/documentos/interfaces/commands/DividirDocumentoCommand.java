6165aea93bf891cdb3da3fee5f1d25dfb6442a74
Tomas.Godoi <tomas.godoi@stf.jus.br>
2016-02-11 16:40:10 -0200

    [PLATAFORMA-481]
    Implementando a união de peças e testes de integração.

--- src/main/java/br/jus/stf/plataforma/documentos/interfaces/commands/DividirDocumentoCommand.java
+++ src/main/java/br/jus/stf/plataforma/documentos/interfaces/commands/DividirDocumentoCommand.java
@@ -1,20 +1,37 @@
 package br.jus.stf.plataforma.documentos.interfaces.commands;
 
+import javax.validation.constraints.NotNull;
+
 import com.wordnik.swagger.annotations.ApiModel;
 import com.wordnik.swagger.annotations.ApiModelProperty;
 
 import br.jus.stf.shared.DocumentoId;
 
+/**
+ * Command para divisão de um documento
+ * 
+ * @author Tomas.Godoi
+ *
+ */
 @ApiModel("Contém as informações necessárias para dividir um documento")
 public class DividirDocumentoCommand {
 
 	@ApiModelProperty("Id do documento a ser dividido")
+	@NotNull
 	private DocumentoId documento;
 	@ApiModelProperty("Página inicial a ser dividida")
+	@NotNull
 	private Integer paginaInicial;
 	@ApiModelProperty("Página final a ser dividida")
+	@NotNull
 	private Integer paginaFinal;
 
+	public DividirDocumentoCommand(DocumentoId documento, Integer paginaInicial, Integer paginaFinal) {
+		this.documento = documento;
+		this.paginaInicial = paginaInicial;
+		this.paginaFinal = paginaFinal;
+	}
+	
 	public DocumentoId getDocumento() {
 		return documento;
 	}
