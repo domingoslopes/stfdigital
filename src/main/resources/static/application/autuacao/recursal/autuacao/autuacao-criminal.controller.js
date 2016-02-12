98680a6758733b47ca6cbd92dc19cb69db9aa42a
Gabriel Teles <gab.teles@hotmail.com>
2016-02-12 17:35:46 -0200

    Permite remover partes da autuação criminal/eleitoral

--- src/main/resources/static/application/autuacao/recursal/autuacao/autuacao-criminal.controller.js
+++ src/main/resources/static/application/autuacao/recursal/autuacao/autuacao-criminal.controller.js
@@ -150,15 +150,8 @@
 				});
 			};
 		
-			partesController.remover = function(parteSelecionada) {
-				parteSelecionada.selected = true;
-				var partesAtuais = partes.slice(0);
-				partesController.clear(partes);
-				angular.forEach(partesAtuais, function(parte) {
-					if (!parte.selected) {
-						partes.push(parte);
-					}
-				});
+			partesController.remover = function(index) {
+				partes.splice(index, 1);
 			};
 			
 			partesController.clear = function(array) {
