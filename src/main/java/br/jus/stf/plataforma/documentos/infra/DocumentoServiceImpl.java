8a942c7a64920ce561772e2d04722386f2221d88
Tomas.Godoi <tomas.godoi@stf.jus.br>
2016-02-12 20:19:39 -0200

    [PLATAFORMA-481]
    Levando as validações de intervalo de divisão para o domínio.

--- src/main/java/br/jus/stf/plataforma/documentos/infra/DocumentoServiceImpl.java
+++ src/main/java/br/jus/stf/plataforma/documentos/infra/DocumentoServiceImpl.java
@@ -3,8 +3,11 @@ package br.jus.stf.plataforma.documentos.infra;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.lang3.Range;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.itextpdf.text.Document;
@@ -16,8 +19,12 @@ import com.itextpdf.text.pdf.RandomAccessFileOrArray;
 
 import br.jus.stf.plataforma.documentos.domain.DocumentoService;
 import br.jus.stf.plataforma.documentos.domain.model.ConteudoDocumento;
+import br.jus.stf.plataforma.documentos.domain.model.Documento;
+import br.jus.stf.plataforma.documentos.domain.model.DocumentoRepository;
 import br.jus.stf.plataforma.documentos.domain.model.DocumentoTemporario;
 import br.jus.stf.plataforma.shared.util.PDFMultipartFile;
+import br.jus.stf.shared.DocumentoId;
+import br.jus.stf.shared.DocumentoTemporarioId;
 
 /**
  * Service para manipulação de documentos do tipo Pdf.
@@ -28,12 +35,16 @@ import br.jus.stf.plataforma.shared.util.PDFMultipartFile;
 @Component
 public class DocumentoServiceImpl implements DocumentoService {
 
+	@Autowired
+	private DocumentoRepository documentoRepository;
+	
 	/**
 	 * Realiza a contagem da quantidade de páginas em um arquivo PDF.
 	 * 
 	 * @param file
 	 * @return
 	 */
+	@Override
 	public Integer contarPaginas(RandomAccessFile file) {
 		try {
 			RandomAccessFileOrArray pdfFile = new RandomAccessFileOrArray(
@@ -93,4 +104,75 @@ public class DocumentoServiceImpl implements DocumentoService {
 			throw new RuntimeException("Erro ao unir os conteúdos dos documentos.", e);
 		}
 	}
+	
+	/**
+	 * Divide um documento nos intervalos especificados.
+	 * 
+	 * @param id
+	 * @param intervalos
+	 * @return
+	 */
+	@Override
+	public List<DocumentoTemporarioId> dividirDocumento(DocumentoId id, List<Range<Integer>> intervalos) {
+		Documento documento = documentoRepository.findOne(id);
+		return dividirDocumento(documento, intervalos);
+	}
+	
+	/**
+	 * Divide um documento contiguamente como especificado pelos intervalos.
+	 * 
+	 * @param id
+	 * @param intervalos
+	 * @return
+	 */
+	@Override
+	public List<DocumentoTemporarioId> dividirDocumentoContiguamente(DocumentoId id, List<Range<Integer>> intervalos) {
+		Documento documento = documentoRepository.findOne(id);
+		if (intervalosSaoContiguos(intervalos, documento)) {
+			return dividirDocumento(documento, intervalos);
+		} else {
+			throw new IllegalArgumentException("Intervalos não são contíguos");
+		}
+	}
+	
+	private List<DocumentoTemporarioId> dividirDocumento(Documento documento, List<Range<Integer>> intervalos) {
+		if (!intervalosSaoContidosPeloDocumento(intervalos, documento)) {
+			throw new IllegalArgumentException("Há intervalos não contidos no documento.");
+		}
+		ConteudoDocumento conteudo = documentoRepository.download(documento.id());
+		List<DocumentoTemporarioId> documentosTemporariosDivididos = new ArrayList<>();
+		for (Range<Integer> intervalo : intervalos) {
+			DocumentoTemporario temp = dividirConteudo(conteudo, intervalo.getMinimum(),
+					intervalo.getMaximum());
+			DocumentoTemporarioId tempId = new DocumentoTemporarioId(documentoRepository.storeTemp(temp));
+			documentosTemporariosDivididos.add(tempId);
+		}
+		return documentosTemporariosDivididos;
+	}
+	
+	private boolean intervalosSaoContiguos(List<Range<Integer>> intervalos, Documento documento) {
+		if (intervalos.get(0).getMinimum() <= 0 || intervalos.get(intervalos.size() - 1).getMaximum() != documento.quantidadePaginas()) {
+			return false;
+		} else {
+			Range<Integer> anterior = intervalos.get(0);
+			for (int i = 1; i < intervalos.size(); i++) {
+				Range<Integer> proximo = intervalos.get(i);
+				if (anterior.getMaximum() + 1 != proximo.getMinimum()) {
+					return false;
+				}
+				anterior = proximo;
+			}
+		}
+		return true;
+	}
+	
+	private boolean intervalosSaoContidosPeloDocumento(List<Range<Integer>> intervalos, Documento documento) {
+		for (Range<Integer> intervalo : intervalos) {
+			if (intervalo.getMinimum() <= 0 || intervalo.getMaximum() > documento.quantidadePaginas()) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
 }
