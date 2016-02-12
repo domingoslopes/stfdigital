f2b3879abeee32212e2ad41bf0b469d7bb1e429a
Tomas.Godoi <tomas.godoi@stf.jus.br>
2016-02-12 16:06:02 -0200

    [PLATAFORMA-481]
    Implementando uma validação dos intervalos.

--- src/main/java/br/jus/stf/plataforma/documentos/application/DocumentoApplicationService.java
+++ src/main/java/br/jus/stf/plataforma/documentos/application/DocumentoApplicationService.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Service;
 
 import br.jus.stf.plataforma.documentos.domain.DocumentoService;
 import br.jus.stf.plataforma.documentos.domain.model.ConteudoDocumento;
+import br.jus.stf.plataforma.documentos.domain.model.Documento;
 import br.jus.stf.plataforma.documentos.domain.model.DocumentoRepository;
 import br.jus.stf.plataforma.documentos.domain.model.DocumentoTemporario;
 import br.jus.stf.shared.DocumentoId;
@@ -66,16 +67,65 @@ public class DocumentoApplicationService {
 	 * @return
 	 */
 	public List<DocumentoId> dividirDocumento(DocumentoId id, List<Range<Integer>> intervalos) {
-		ConteudoDocumento conteudo = documentoRepository.download(id);
+		Documento documento = documentoRepository.findOne(id);
+		return dividirDocumento(documento, intervalos);
+	}
+	
+	private List<DocumentoId> dividirDocumento(Documento documento, List<Range<Integer>> intervalos) {
+		if (!intervalosSaoContidosPeloDocumento(intervalos, documento)) {
+			throw new IllegalArgumentException("Há intervalos não contidos no documento.");
+		}
+		ConteudoDocumento conteudo = documentoRepository.download(documento.id());
 		List<DocumentoId> documentosDivididos = new ArrayList<>();
 		for (Range<Integer> intervalo : intervalos) {
 			DocumentoTemporario temp = documentoService.dividirConteudo(conteudo, intervalo.getMinimum(),
-			        intervalo.getMaximum());
+					intervalo.getMaximum());
 			DocumentoTemporarioId tempId = new DocumentoTemporarioId(documentoRepository.storeTemp(temp));
 			DocumentoId novoDocumento = documentoRepository.save(tempId);
 			documentosDivididos.add(novoDocumento);
 		}
 		return documentosDivididos;
+	}
+	
+	private boolean intervalosSaoContidosPeloDocumento(List<Range<Integer>> intervalos, Documento documento) {
+		for (Range<Integer> intervalo : intervalos) {
+			if (intervalo.getMinimum() <= 0 || intervalo.getMaximum() > documento.quantidadePaginas()) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	/**
+	 * Divide um documento contiguamente como especificado pelos intervalos.
+	 * 
+	 * @param id
+	 * @param intervalos
+	 * @return
+	 */
+	public List<DocumentoId> dividirDocumentoContiguamente(DocumentoId id, List<Range<Integer>> intervalos) {
+		Documento documento = documentoRepository.findOne(id);
+		if (intervalosSaoContiguos(intervalos, documento)) {
+			return dividirDocumento(documento, intervalos);
+		} else {
+			throw new IllegalArgumentException("Intervalos não são contíguos");
+		}
+	}
+
+	private boolean intervalosSaoContiguos(List<Range<Integer>> intervalos, Documento documento) {
+		if (intervalos.get(0).getMinimum() <= 0 || intervalos.get(intervalos.size() - 1).getMaximum() != documento.quantidadePaginas()) {
+			return false;
+		} else {
+			Range<Integer> anterior = intervalos.get(0);
+			for (int i = 1; i < intervalos.size(); i++) {
+				Range<Integer> proximo = intervalos.get(i);
+				if (anterior.getMaximum() + 1 != proximo.getMinimum()) {
+					return false;
+				}
+				anterior = proximo;
+			}
+		}
+		return true;
 	}
 
 	/**
