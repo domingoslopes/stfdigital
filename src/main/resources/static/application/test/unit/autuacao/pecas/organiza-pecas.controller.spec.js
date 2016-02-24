687dc7e8b8925f237bb437f136236f974b19b8c9
viniciusksilva <viniciusksilva@gmail.com>
2016-02-24 18:00:05 -0300

    plataforma-502 teste unitario corrigido da funcionalidade de organizar pecas

--- src/main/resources/static/application/test/unit/autuacao/pecas/organiza-pecas.controller.spec.js
+++ src/main/resources/static/application/test/unit/autuacao/pecas/organiza-pecas.controller.spec.js
@@ -11,33 +11,40 @@
 	var mockProcesso = {
 		assuntos: [],
 		classe: "RE",
-		id: 10,
-		identificacao: "RE 7",
-		numero: 7,
-		observacaoAnalise: null,
-		partes: {'Polo Ativo': [], 'Polo Passivo': []},
+		id: 8,
+		identificacao: "RE 5",
+		numero: 5,
+		observacaoAnalise: "123",
+		partes: {PoloAtivo: [], PoloPassivo: []},
 		pecas: [{id: 1, tipoPecaId : 2, visibilidade: 'P', situacao: 'JUNTADA', descricao: 'peca juntada do processo RE 7'}, 
-		        {id: 2, tipoPecaId : 1, visibilidade: 'I', situacao: 'ASSINADA', descricao: 'peca assinada do processo RE 7'}]
+		        {id: 2, tipoPecaId : 1, visibilidade: 'I', situacao: 'ASSINADA', descricao: 'peca assinada do processo RE 7'}],
+		preferencias: [],
+		relator: null,
+		situacao: "A analisar",
+		teses: []
 	};
 	
-	describe('Controle das peças do processo selecionado', function() {
+	
+	describe('Organizar Peças Controller', function() {
+		var fakeData = [];
+		var stateParams = [];
 		var controller;
+		var scope;
+		var createController;
 		var $httpBackend;
-		var properties;
 		
 		beforeEach(module('appDev'));
 		
-		beforeEach(inject(function(_$httpBackend_, $rootScope, $controller, $state, messages, _properties_, ClasseService, PeticaoService) {
+		beforeEach(inject(function(_$httpBackend_, $rootScope, $controller, $log, messages, properties, $state, ProcessoService) {
 			$httpBackend = _$httpBackend_;
-			properties = _properties_;
 			
-			$httpBackend.expectGET(properties.apiUrl + '/processo/1/pecas').respond(mockProcesso);
+			$httpBackend.expectGET(properties.apiUrl + '/peticoes/14/processo').respond(mockProcesso);
 			
-			var scope = $rootScope.$new();
-			
+			scope = $rootScope.$new();
 			controller = $controller('OrganizaPecasController', {
 				$scope: scope,
-				$stateParams: {resources: [18]},
+				$stateParams: {resources: [14]},
+				$log: $log,
 				messages: messages,
 				properties: properties,
 				$state: $state,
@@ -50,48 +57,17 @@
 		it('Deveria inicializar o controlador', function() {
 			expect(controller).not.toBeNull();
 		});
-
 		
-		it('Deveria recuperar as pecas do processo', function() {
-			var pecas = [];
-			pecas = controller.processo.pecas;
-			expect(pecas.length).toBeGreaterThan(0);
+		it('Deveria carregar informações do processo', function() {
+			expect(controller.processo).toEqual(mockProcesso);
 		});
 		
-		/*it('Deveria não adicionar um assunto repetido à lista', function() {
-			var qtdAssuntos = controller.assuntosSelecionados.length;
-			var assunto = {id: "2662", descricao: "Férias"};
-			controller.adicionarAssuntoNaLista(assunto);
-			controller.adicionarAssuntoNaLista(assunto);
-			expect(controller.assuntosSelecionados.length).toBe(qtdAssuntos + 1);
+		it('Deveria existir peças no processo', function() {
+			expect(controller.processo.pecas.length).toBeGreaterThan(0);;
 		});
 		
-		it('Deveria remover um assunto da lista', function() {
-			var qtdAssuntos = controller.assuntosSelecionados.length;
-			var assunto = {id: "2662", descricao: "Férias"};
-			controller.adicionarAssuntoNaLista(assunto);
-			controller.removerAssuntoSelecionadoLista(0);
-			expect(controller.assuntosSelecionados.length).toBe(qtdAssuntos);
+		it('Deveria validar o formulário quando os parâmetros não forem alterados', function() {
+			expect(controller.validar()).toBeTruthy;
 		});
-		
-		it('Deveria não validar a autuação quando não houver partes', function() {
-			expect(controller.validar()).toBeFalsy();
-		});
-		
-		it('Deveria não validar a autuação quando não houver partes do polo ativo', function() {
-			controller.adicionarPoloPassivo('José');
-			expect(controller.validar()).toBeFalsy();
-		});
-		
-		it('Deveria não validar a autuação quando não houver partes do polo passivo', function() {
-			controller.adicionarPoloAtivo('João');
-			expect(controller.validar()).toBeFalsy();
-		});
-		
-		it('Deveria validar a autuação quando houver partes do polo ativo e passivo', function() {
-			controller.adicionarPoloAtivo('João');
-			controller.adicionarPoloPassivo('José');
-			expect(controller.validar()).toBeTruthy();
-		});*/
 	});
 })();
