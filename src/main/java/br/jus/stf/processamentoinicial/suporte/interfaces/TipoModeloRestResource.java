a2a8f72f3213854696043d2dcb8a95052fdf1c04
Tomas.Godoi <tomas.godoi@stf.jus.br>
2016-03-08 00:49:23 -0300

    Adicionando serviços de tipo de modelo.

---
+++ src/main/java/br/jus/stf/processamentoinicial/suporte/interfaces/TipoModeloRestResource.java
@@ -0,0 +1,34 @@
+package br.jus.stf.processamentoinicial.suporte.interfaces;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.wordnik.swagger.annotations.ApiOperation;
+
+import br.jus.stf.processamentoinicial.suporte.domain.model.TipoModeloRepository;
+import br.jus.stf.processamentoinicial.suporte.interfaces.dto.TipoModeloDto;
+import br.jus.stf.processamentoinicial.suporte.interfaces.dto.TipoModeloDtoAssembler;
+
+@RestController
+@RequestMapping("/api/tipos-modelo")
+public class TipoModeloRestResource {
+
+	@Autowired
+	private TipoModeloRepository tipoModeloRepository;
+
+	@Autowired
+	private TipoModeloDtoAssembler tipoModeloDtoAssembler;
+
+	@ApiOperation("Recupera os tipos de modelo cadastrados")
+	@RequestMapping(value = "", method = RequestMethod.GET)
+	public List<TipoModeloDto> listar() {
+		return tipoModeloRepository.findAll().stream().map(tm -> tipoModeloDtoAssembler.toDto(tm))
+		        .collect(Collectors.toList());
+	}
+
+}
