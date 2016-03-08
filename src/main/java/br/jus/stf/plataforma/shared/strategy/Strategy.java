45bee7f16f84917b16671a2fde21696e38f45cf4
Tomas.Godoi <tomas.godoi@stf.jus.br>
2016-03-08 00:10:12 -0300

    Criando classes para um mecanismo de Strategy reutilizável.

---
+++ src/main/java/br/jus/stf/plataforma/shared/strategy/Strategy.java
@@ -0,0 +1,12 @@
+package br.jus.stf.plataforma.shared.strategy;
+
+/**
+ * Interface de marcação para Strategies.
+ * 
+ * @author Tomas.Godoi
+ *
+ * @param <S>
+ */
+public interface Strategy<S> {
+
+}
---
+++ src/main/java/br/jus/stf/plataforma/shared/strategy/UnresolvedStrategyException.java
@@ -0,0 +1,19 @@
+package br.jus.stf.plataforma.shared.strategy;
+
+/**
+ * Exceção a ser lançada quando uma Strategy não for encontrada.
+ * 
+ * @author Tomas.Godoi
+ *
+ */
+public class UnresolvedStrategyException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	private static final String MESSAGE = "Strategy não encontrada para o identificador %s";
+
+	public UnresolvedStrategyException(String identifier) {
+		super(String.format(MESSAGE, identifier));
+	}
+
+}
