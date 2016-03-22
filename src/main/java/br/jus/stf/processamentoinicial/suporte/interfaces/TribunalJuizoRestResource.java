f100046dc3d105993dedb6a057474e06eecf0be0
Anderson.Araujo <anderson.araujo@stf.jus.br>
2016-03-22 18:52:21 -0300

    Criação de uma operação para retornar os tribunais/juízos de acordo com a origem informada.

--- src/main/java/br/jus/stf/processamentoinicial/suporte/interfaces/TribunalJuizoRestResource.java
+++ src/main/java/br/jus/stf/processamentoinicial/suporte/interfaces/TribunalJuizoRestResource.java
@@ -5,11 +5,14 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.jus.stf.processamentoinicial.suporte.domain.model.ProcedenciaGeograficaRepository;
 import br.jus.stf.processamentoinicial.suporte.domain.model.TribunalJuizoRepository;
+import br.jus.stf.processamentoinicial.suporte.domain.model.UnidadeFederacao;
 import br.jus.stf.processamentoinicial.suporte.interfaces.dto.TribunalJuizoDto;
 import br.jus.stf.processamentoinicial.suporte.interfaces.dto.TribunalJuizoDtoAssembler;
 
@@ -30,12 +33,23 @@ public class TribunalJuizoRestResource {
 	private TribunalJuizoRepository tribunalJuizoRepository;
 	
 	@Autowired
+	ProcedenciaGeograficaRepository procedenciaGeograficaRepository;
+	
+	@Autowired
 	private TribunalJuizoDtoAssembler tribunalJuizoDtoAssembler; 
 	
 	@ApiOperation("Retorna uma lista de juízos de origem")
 	@RequestMapping(value = "", method = RequestMethod.GET)
 	public List<TribunalJuizoDto> listar(){
 		return Optional.ofNullable(tribunalJuizoRepository.findAll().stream().map(t -> tribunalJuizoDtoAssembler.toDo(t))
+				.collect(Collectors.toList())).orElseThrow(IllegalArgumentException::new);
+	}
+	
+	@ApiOperation("Retorna uma lista de juízos de origem")
+	@RequestMapping(value = "/origens/{id}", method = RequestMethod.GET)
+	public List<TribunalJuizoDto> consultarPorOrigem(@PathVariable("id") Long id){
+		UnidadeFederacao uf = procedenciaGeograficaRepository.findOneUnidadeFederacao(id);
+		return Optional.ofNullable(tribunalJuizoRepository.findByUnidadeFederacao(uf) .stream().map(t -> tribunalJuizoDtoAssembler.toDo(t))
 				.collect(Collectors.toList())).orElseThrow(IllegalArgumentException::new);
 	}
 }
