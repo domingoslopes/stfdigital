dc5b8ef2fcc0ce32ab08d0940045edb6144e1ac0
Tomas.Godoi <tomas.godoi@stf.jus.br>
2016-05-17 19:41:19 -0300

    Adicionando script para instalar certificado do registro docker.

---
+++ install-registry-certificate.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+echo -n | openssl s_client -connect docker-registry.stf.jus.br:443 -showcerts 2>/dev/null | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > stfca.pem
+
+docker-machine scp stfca.pem default:/tmp/stfca.pem
+docker-machine ssh default 'sudo mkdir -p /var/lib/boot2docker/certs'
+docker-machine ssh default 'sudo mv /tmp/stfca.pem /var/lib/boot2docker/certs/stfca.pem'
+echo "Reinicie a máquina com 'docker-machine restart default'"
