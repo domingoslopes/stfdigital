3e124567e2a9854f51f42f088940f7334ee1bc67
Tomás Godoi <tsmgodoi@gmail.com>
2016-06-27 13:18:32 -0300

    Alterando a porta do nexus para 9981, de modo que não conflite com porta da aplicação.

--- build-parallely-with-registry-cache.sh
+++ build-parallely-with-registry-cache.sh
@@ -2,7 +2,7 @@
 
 build_parallely() {
     ./start-npm-proxy.sh
-    gradle docker -PnpmRegistry="http://docker:8081/content/groups/npm/"
+    gradle docker -PnpmRegistry="http://docker:9981/content/groups/npm/"
 }
 
 (time build_parallely) 2>&1 | tee build-parallely.log
--- start-npm-proxy.sh
+++ start-npm-proxy.sh
@@ -10,7 +10,7 @@ if [ "$EXISTS_NEXUS_CONTAINER" ]; then
 else
     echo 'Criando e iniciando o container Nexus'
     docker create --name $NEXUS_CONTAINER-data sonatype/nexus
-    docker run -d -p 8081:8081 --name $NEXUS_CONTAINER --volumes-from $NEXUS_CONTAINER-data sonatype/nexus
+    docker run -d -p 9981:8081 --name $NEXUS_CONTAINER --volumes-from $NEXUS_CONTAINER-data sonatype/nexus
 fi
 
 echo 'Aguardando o proxy iniciar'
@@ -18,6 +18,6 @@ docker run --rm --link $NEXUS_CONTAINER:$NEXUS_CONTAINER aanand/wait
 
 if [ ! "$EXISTS_NEXUS_CONTAINER" ]; then
     echo 'Criando o repositório proxy do https://registry.npmjs.org'
-    curl -H "Content-Type:application/json" -d @nexus-npm-cache.json -X POST -u admin:admin123 http://docker:8081/service/local/repositories
-    curl -H "Content-Type:application/json" -d @nexus-npm-group.json -X POST -u admin:admin123 http://docker:8081/service/local/repo_groups
+    curl -H "Content-Type:application/json" -d @nexus-npm-cache.json -X POST -u admin:admin123 http://docker:9981/service/local/repositories
+    curl -H "Content-Type:application/json" -d @nexus-npm-group.json -X POST -u admin:admin123 http://docker:9981/service/local/repo_groups
 fi
