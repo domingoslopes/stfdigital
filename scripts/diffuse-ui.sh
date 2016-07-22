a612310f593fc466e8a35755808577136cd3867e
Tomas.Godoi <tomas.godoi@stf.jus.br>
2016-07-22 03:11:54 -0300

    Script diffuse para ui.

---
+++ scripts/diffuse-ui.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+projects_ui=("plataforma/documents" \
+    "plataforma/services" \
+    "plataforma/userauthentication" \
+    "autuacao/autuacao" \
+    "autuacao/distribuicao" \
+    "autuacao/peticionamento" \
+    "autuacao/recebimento"
+)
+
+command="diffuse "
+
+for project in "${projects_ui[@]}"
+do
+    command="$command $project/$@"
+done
+
+eval $command
